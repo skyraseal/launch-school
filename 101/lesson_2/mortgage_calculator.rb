d89d287c63f03d50623e227dfdb0d8b1de69830e
Mathew Nguyen <skyraxseal@yahoo.com>
2021-06-28 13:15:04 -0500

    improvements to the mortgage calculator

--- 101/lesson_2/mortgage_calculator.rb
+++ 101/lesson_2/mortgage_calculator.rb
@@ -4,11 +4,16 @@ MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
 LANGUAGE = 'en'
 
 def prompt(message)
-  Kernel.puts(">> #{message}")
+  puts(">> #{message}")
 end
 
 def valid_number?(num)
-  /^[\-+]?[0-9]?[0-9.]*$/.match?(num) && num.count(".") <= 1 && num.count("0-9") > 0
+  /^[\-+]?[0-9]?[0-9.]*$/.match?(num) &&
+    num.count(".") <= 1 && num.count("0-9") > 0
+end
+
+def negative_num?(num)
+  /^[\-]/.match?(num)
 end
 
 def request_loan_amount
@@ -16,8 +21,8 @@ def request_loan_amount
   loan_amount = 0
 
   loop do
-    loan_amount = Kernel.gets().chomp()
-    break if valid_number?(loan_amount)
+    loan_amount = gets.chomp
+    break if valid_number?(loan_amount) && !negative_num?(loan_amount)
     prompt(MESSAGES[LANGUAGE]['incorrect_loan_amt_entry'])
   end
 
@@ -29,8 +34,8 @@ def request_loan_apr
   loan_apr = 0
 
   loop do
-    loan_apr = Kernel.gets().chomp()
-    break if valid_number?(loan_apr)
+    loan_apr = gets.chomp
+    break if valid_number?(loan_apr) && !negative_num?(loan_apr)
     prompt(MESSAGES[LANGUAGE]['incorrect_loan_apr_entry'])
   end
 
@@ -42,19 +47,25 @@ def request_loan_duration
   loan_duration = 0
 
   loop do
-    loan_duration = Kernel.gets().chomp()
-    break if valid_number?(loan_duration)
+    loan_duration = gets.chomp
+    break if valid_number?(loan_duration) && !negative_num?(loan_duration)
     prompt(MESSAGES[LANGUAGE]['incorrect_loan_duration_entry'])
   end
 
   loan_duration
 end
 
-def request_try_again
+def request_try_again?
   prompt(MESSAGES[LANGUAGE]['try_again'])
-  try_again = gets.chomp.downcase
+  try_again = ''
 
-  ['y', 'yes'].include?(try_again)
+  loop do
+    try_again = gets.chomp.downcase
+    break if ['y', 'yes', 'ye', 'n', 'no'].include?(try_again)
+    prompt(MESSAGES[LANGUAGE]['incorrect_try_again_entry'])
+  end
+
+  ['y', 'ye', 'yes'].include?(try_again)
 end
 
 def calc_monthly_mortgage(loan_amount, loan_apr, loan_duration)
@@ -64,7 +75,13 @@ def calc_monthly_mortgage(loan_amount, loan_apr, loan_duration)
 
   loan_monthly_interest = loan_apr / 12
 
-  monthly_mortgage = loan_amount * (loan_monthly_interest / (1 - ((1 + loan_monthly_interest)**-loan_duration)))
+  if loan_apr == 0
+    monthly_mortgage = loan_amount / loan_duration
+  else
+    monthly_mortgage = loan_amount * (loan_monthly_interest /
+      (1 - ((1 + loan_monthly_interest)**-loan_duration)))
+  end
+
   monthly_mortgage.round(2)
 end
 
@@ -73,6 +90,7 @@ loan_apr = 0
 loan_duration = 0
 monthly_mortgage = 0
 
+system("clear")
 prompt(MESSAGES[LANGUAGE]['welcome'])
 loop do
   loan_amount = request_loan_amount
@@ -83,9 +101,10 @@ loop do
   prompt(MESSAGES[LANGUAGE]['calculating'])
   monthly_mortgage = calc_monthly_mortgage(loan_amount, loan_apr, loan_duration)
   prompt(MESSAGES[LANGUAGE]['output_result'])
-  Kernel.puts(monthly_mortgage)
+  puts(monthly_mortgage)
 
-  break unless request_try_again
+  break unless request_try_again?
+  system("clear")
 end
 
 prompt(MESSAGES[LANGUAGE]['goodbye'])
