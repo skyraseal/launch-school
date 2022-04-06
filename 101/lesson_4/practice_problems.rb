f9115df679344e75ba6cec28dd46b4871c93a671
Mathew Nguyen <skyraxseal@yahoo.com>
2022-04-06 18:31:37 -0500

    finished lesson 4 prac problems

--- 101/lesson_4/practice_problems.rb
+++ 101/lesson_4/practice_problems.rb
@@ -153,3 +153,32 @@ numbers.each do |number|
   numbers.shift(1)
 end
 # A: Not sure without testing, but you should never modify something you're iterating over.
+
+# 9
+# Q: Write your own version of the rails titleize implementation
+# A:
+def titleize(title)
+  title.split.map {|word| word.capitalize}.join(' ')
+end
+
+# 10
+# Q: Modify the hash such that each member of the Munster family 
+# has an additional "age_group" key that has one of three values describing 
+# the age group the family member is in (kid, adult, or senior). 
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+# A:
+munsters.each do |person, demographic|
+  demographic['age_group'] = if demographic['age'] <= 18
+                               'kid'
+                             elsif demographic['age'] < 60
+                               'adult'
+                             else
+                               'senior'
+                             end
+end
